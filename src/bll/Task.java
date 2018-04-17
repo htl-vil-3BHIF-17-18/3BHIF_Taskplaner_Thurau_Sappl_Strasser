2a009b414ea978e7a3a9a7bf6929ba190bcb5d3c
schueler <schueler@ORTNAAAAAA>
2018-04-17 10:33:33 +0200

    Task Klasse erstellt

--- src/bll/Task.java
+++ src/bll/Task.java
@@ -1,5 +1,99 @@
 package bll;
 
+import java.util.Date;
+
 public class Task {
+	private String text;
+	private Date datum;
+	private String typ;
+	private String fach;
+
+	public Task(String text, Date datum, String typ, String fach) {
+		super();
+		this.text = text;
+		this.datum = datum;
+		this.typ = typ;
+		this.fach = fach;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Date getDatum() {
+		return datum;
+	}
+
+	public void setDatum(Date datum) {
+		this.datum = datum;
+	}
+
+	public String getTyp() {
+		return typ;
+	}
+
+	public void setTyp(String typ) {
+		this.typ = typ;
+	}
+
+	public String getFach() {
+		return fach;
+	}
+
+	public void setFach(String fach) {
+		this.fach = fach;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result + ((fach == null) ? 0 : fach.hashCode());
+		result = prime * result + ((text == null) ? 0 : text.hashCode());
+		result = prime * result + ((typ == null) ? 0 : typ.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Task other = (Task) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (fach == null) {
+			if (other.fach != null)
+				return false;
+		} else if (!fach.equals(other.fach))
+			return false;
+		if (text == null) {
+			if (other.text != null)
+				return false;
+		} else if (!text.equals(other.text))
+			return false;
+		if (typ == null) {
+			if (other.typ != null)
+				return false;
+		} else if (!typ.equals(other.typ))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Task [text=" + text + ", datum=" + datum + ", typ=" + typ + ", fach=" + fach + "]";
+	}
 
 }
