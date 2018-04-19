d96bd87b4d6de900367be275cb3548298becdf20
schueler <schueler@NB-R85DPGW>
2018-04-19 08:24:14 +0200

    Mainframe erzeugen

--- src/app/Start.java
+++ src/app/Start.java
@@ -1,9 +1,11 @@
 package app;
 
+import gui.MainFrame;
+
 public class Start {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MainFrame mf = new MainFrame("Taskplaner");
 
 	}
 
