35737c7d7a39241e3a6efbe3e347d7a6cc06b65b
ich <alex.thurau.at@gmail.com>
2018-04-23 10:04:35 +0200

    nix

--- src/bll/Task.java
+++ src/bll/Task.java
@@ -2,22 +2,24 @@ package bll;
 
 import java.util.Date;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import dal.DatabaseMapable;
 
-public class Task implements DatabaseMapable {
+public class Task implements DatabaseMapable, Comparable<Task> {
 	private String text;
-	private Date datum;
+	private Date datumBis;
+	private Date datumVon;
 	private String typ;
 	private String fach;
+	private boolean erledigt;
 
-	public Task(String text, Date datum, String typ, String fach) {
+	public Task(String text, Date datumVon, Date datumBis, String typ, String fach) {
 		super();
 		this.text = text;
-		this.datum = datum;
+		this.datumVon = datumVon;
+		this.datumBis = datumBis;
 		this.typ = typ;
 		this.fach = fach;
 	}
@@ -30,12 +32,20 @@ public class Task implements DatabaseMapable {
 		this.text = text;
 	}
 
-	public Date getDatum() {
-		return datum;
+	public Date getDatumBis() {
+		return datumBis;
 	}
 
-	public void setDatum(Date datum) {
-		this.datum = datum;
+	public void setDatumBis(Date datumBis) {
+		this.datumBis = datumBis;
+	}
+
+	public Date getDatumVon() {
+		return datumVon;
+	}
+
+	public void setDatumVon(Date datumVon) {
+		this.datumVon = datumVon;
 	}
 
 	public String getTyp() {
@@ -58,7 +68,9 @@ public class Task implements DatabaseMapable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result + ((datumBis == null) ? 0 : datumBis.hashCode());
+		result = prime * result + ((datumVon == null) ? 0 : datumVon.hashCode());
+		result = prime * result + (erledigt ? 1231 : 1237);
 		result = prime * result + ((fach == null) ? 0 : fach.hashCode());
 		result = prime * result + ((text == null) ? 0 : text.hashCode());
 		result = prime * result + ((typ == null) ? 0 : typ.hashCode());
@@ -74,10 +86,17 @@ public class Task implements DatabaseMapable {
 		if (getClass() != obj.getClass())
 			return false;
 		Task other = (Task) obj;
-		if (datum == null) {
-			if (other.datum != null)
+		if (datumBis == null) {
+			if (other.datumBis != null)
 				return false;
-		} else if (!datum.equals(other.datum))
+		} else if (!datumBis.equals(other.datumBis))
+			return false;
+		if (datumVon == null) {
+			if (other.datumVon != null)
+				return false;
+		} else if (!datumVon.equals(other.datumVon))
+			return false;
+		if (erledigt != other.erledigt)
 			return false;
 		if (fach == null) {
 			if (other.fach != null)
@@ -99,11 +118,13 @@ public class Task implements DatabaseMapable {
 
 	@Override
 	public String toString() {
-		return "Task [text=" + text + ", datum=" + datum + ", typ=" + typ + ", fach=" + fach + "]";
+		return "Task [text=" + text + ", datumBis=" + datumBis + ", datumVon=" + datumVon + ", typ=" + typ + ", fach="
+				+ fach + ", erledigt=" + erledigt + "]";
 	}
 
 	/**
 	 * Gibt eine Map (Key: Feld, Value: Spaltenname) zurück.
+	 * 
 	 * @return {@link Map}
 	 */
 	@Override
@@ -111,9 +132,24 @@ public class Task implements DatabaseMapable {
 		Map<String, String> columnField = new HashMap<String, String>();
 		columnField.put("text", "text");
 		columnField.put("datum", "date");
+		columnField.put("datumVon", "date");
+		columnField.put("datumBis", "date");
 		columnField.put("typ", "type");
 		columnField.put("fach", "subject");
 		return columnField;
+	}
+
+	@Override
+	public int compareTo(Task o) {
+		return this.datumVon.compareTo(o.getDatumVon());
+	}
+
+	public boolean isErledigt() {
+		return erledigt;
+	}
+
+	public void setErledigt(boolean erledigt) {
+		this.erledigt = erledigt;
 	}
 
 }
---
+++ src/gui/TaskTable.java
@@ -0,0 +1,7 @@
+package gui;
+
+import javax.swing.JTable;
+
+public class TaskTable extends JTable {
+
+}
